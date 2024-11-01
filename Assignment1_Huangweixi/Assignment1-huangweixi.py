9373196a6219a97315d086c7530259ad97aac071
delta <weixihuang@ln.hk>
2024-11-01 11:40:23 +0800

    Update Assignment1-huangweixi.py

--- Assignment1_Huangweixi/Assignment1-huangweixi.py
+++ Assignment1_Huangweixi/Assignment1-huangweixi.py
@@ -95,7 +95,7 @@ while True:
 
             # compute both the starting and ending coordinates for the text prediction bounding box
             # use rotation matrix:(x′=xcos(θ)−ysin(θ),y′=xsin(θ)+ycos(θ)) to calculate the coordinates after possible spin
-            endX = int(offsetX + (cos * xData1[x]) + (sin * xData2[x]))
+            endXxx = int(offsetX + (cos * xData1[x]) + (sin * xData2[x]))
             endY = int(offsetY - (sin * xData1[x]) + (cos * xData2[x]))
             startX = int(endX - box_w)
             startY = int(endY - box_h)
